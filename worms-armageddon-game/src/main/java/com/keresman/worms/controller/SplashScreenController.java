a70ea2fab44c6b88b8eb4dfae291540d4f007652
Josip <josipkeresman@gmail.com>
2025-10-18 22:28:12 +0200

    Remove unused imports

--- worms-armageddon-game/src/main/java/com/keresman/worms/controller/SplashScreenController.java
+++ worms-armageddon-game/src/main/java/com/keresman/worms/controller/SplashScreenController.java
@@ -1,15 +1,8 @@
 package com.keresman.worms.controller;
 
 import com.keresman.worms.utility.MessageUtils;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.stage.Stage;
 
 public class SplashScreenController {
-
-    @FXML
-    private Button btnMilica;
 
     public void onButtonClick() {
         MessageUtils.showInfoMessage("Milica na plazi!");
