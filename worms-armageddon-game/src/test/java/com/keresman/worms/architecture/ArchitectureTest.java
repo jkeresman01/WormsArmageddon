467bc66712f612e0eb58d2b646d9640be445a818
Josip <josipkeresman@gmail.com>
2025-10-19 14:35:52 +0200

    Remove general architecture tests (#2)

--- worms-armageddon-game/src/test/java/com/keresman/worms/architecture/ArchitectureTest.java
+++
@@ -1,8 +0,0 @@
-package com.keresman.worms.architecture;
-
-import com.tngtech.archunit.junit.AnalyzeClasses;
-
-@AnalyzeClasses(packages = "com.keresman.worms")
-public class ArchitectureTest {
-
-}
